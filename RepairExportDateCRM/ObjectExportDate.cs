455d714b40c408d7598c0aa74cdac46a0d387571
Nick Plehanov <nick.plehanov74@gmail.com>
2020-10-13 11:52:06 +0500

    Добавьте файлы проекта.

---
+++ RepairExportDateCRM/ObjectExportDate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepairExportDateCRM {
+	class ObjectExportDate {
+		public DateTime? ExportDate { get; set; }
+		public int? NumberObject { get; set; }
+	}
+}
---
+++ RepairExportDateCRM/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace RepairExportDateCRM {
+	class Program {
+		
+		static void Main(string[] args) {
+			List<ObjectExportDate> objectExportDates = new List<ObjectExportDate>();
+			string path = @"\\Server-nas\общее\Applications\ExportFromCrmTo1C\History";
+			string[] files = System.IO.Directory.GetFiles(path);
+			if(files.Length > 0) {
+				foreach(string item in files) {
+					//var xml = XDocument.Load(item);
+					//var query = from c in xml.Root.Descendants(@"Контрагенты")
+					//			//where (int)c.Attribute("id") < 4
+					//			select c.Element("НомерОбъекта").Value;
+					//foreach(var it in query) {
+					//	Console.WriteLine(it);
+					//	Console.ReadKey();
+					//}
+					XmlDocument doc = new XmlDocument();
+					doc.Load(item);
+					XmlNode node = doc.DocumentElement.SelectSingleNode("/Контрагенты/Контрагент/Объекты/Объект");
+					if(node != null) {
+						string attr = node.ChildNodes[2].InnerText;
+						if(!string.IsNullOrEmpty(attr))
+							objectExportDates.Add(new ObjectExportDate() {
+								ExportDate = DateTime.Parse(item.Substring(item.LastIndexOf(@"\"), 11).Replace(@"\", " ").Trim()),
+								NumberObject = int.Parse(attr)
+							});
+						Console.WriteLine("{0} - {1}", DateTime.Parse(item.Substring(item.LastIndexOf(@"\"), 11).Replace(@"\", " ").Trim()).ToShortDateString(), attr);
+					}
+				}
+			}
+		}
+	}
+}
